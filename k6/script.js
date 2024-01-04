0a2692bc826c7bc2521f9dd6eeb99ccdfa710ad0
Mario K. Shimao <mario.shimao@aglsolutions.com.br>
2024-01-04 18:00:52 -0300

    k6 script

--- k6/script.js
+++ k6/script.js
@@ -1,10 +1,26 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
+import { randomIntBetween, randomItem, randomString } from "https://jslib.k6.io/k6-utils/1.2.0/index.js";
 export const options = {
-  vus: 30,
-  duration: '300s',
+  vus: 10,
+  duration: '10s',
 };
 export default function () {
   http.get('http://localhost:8888/Core/ClientAPI/List');
   sleep(1);
+  const id = randomIntBetween(2, 1000);
+  const name = randomString(10);
+  const adrresss = randomString(10);
+  const payload = JSON.stringify({
+    ClientId: id,
+    ClientName: name,
+    ClientAddress: adrresss
+  });
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  const resp = http.post(`http://localhost:8888/Core/ClientAPI/Insert`, payload, params); 
+  console.log(resp.body);
 }
